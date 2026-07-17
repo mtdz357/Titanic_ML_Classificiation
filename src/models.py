5543b4083bb3e48c055063c202970c542d19e1f2
Murat Dogmaz <mrtdgmz21@gmail.com>
2026-07-17 13:14:33 +0300

    tqdm ilerleme cubugu eklendi

--- src/models.py
+++ src/models.py
@@ -3,7 +3,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
-
+from tqdm import tqdm
 
 def get_models():
     models={
@@ -18,7 +18,7 @@ def get_models():
 
 def train_evalute_models(model,X_train,X_test,y_train,y_test):
     results ={}
-    for name_models,models in model.items():
+    for name_models,models in tqdm(model.items(),desc='Modeller Egitiliyor'):
         models.fit(X_train,y_train)
         score = models.score(X_test,y_test)
         results[name_models]=score
